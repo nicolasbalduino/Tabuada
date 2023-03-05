b44996ea38f3e372570e29931c0eb1fe199f51dc
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 15:59:35 -0300

    Add project files.

---
+++ Tabuada/Program.cs
@@ -0,0 +1,12 @@
+﻿int num;
+
+do
+{
+    Console.WriteLine("Informe um numero de 1 a 9 para fazer a tabuada: ");
+    num = int.Parse(Console.ReadLine());
+} while (num < 1 || num > 9);
+
+for (int i = 0; i < 11; i++)
+{
+    Console.WriteLine(num + "x" + i + "=" + num * i);
+}
