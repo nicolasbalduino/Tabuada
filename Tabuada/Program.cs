fd8f3675516d01954fdc06c203e58cd3ffa574c0
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 21:00:11 -0300

    criada a funcao que calcula a tabuada do numero informado pelo usuario

--- Tabuada/Program.cs
+++ Tabuada/Program.cs
@@ -1,12 +1,17 @@
 ﻿int num;
 
+void CalcularTabuada()
+{
+    for (int i = 0; i < 11; i++)
+    {
+        Console.WriteLine(num + "x" + i + "=" + num * i);
+    }
+}
+
 do
 {
     Console.WriteLine("Informe um numero de 1 a 9 para fazer a tabuada: ");
     num = int.Parse(Console.ReadLine());
 } while (num < 1 || num > 9);
 
-for (int i = 0; i < 11; i++)
-{
-    Console.WriteLine(num + "x" + i + "=" + num * i);
-}
+CalcularTabuada();
